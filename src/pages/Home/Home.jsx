c9feb67d02984c1d3a95d69e73acebdd63506753
ajoy45 <ajoypust@gmail.com>
2023-05-03 15:17:43 +0600

    contact section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 import ChefCard from './ChefCard';
 import { TbChefHat } from "react-icons/tb";
 import Team from './Team';
+import Contact from './Contact';
 const Home = () => {
     const [isLoading,setIsLoading]=useState(true);
     const datas = useLoaderData();
@@ -27,7 +28,7 @@ const Home = () => {
                         <div className="divider"><TbChefHat ></TbChefHat></div>
                     </div>
 
-                <p className='px-12 text-xl'>Indian Chef Take-Out Express Motto is to bring to your home the aroma of India's rich culture, flavor, and culinary heritage. The eatery features selections of Indian meals to choose from and is the best choice for tasty Indian dishes. They come up with great and flavorful meals at worth-able prices, with outstanding experiences for patrons of all ages and historic towns like Umatilla. Whether you're craving a quick bite or looking to treat yourself to a delicious meal, you can order your Indian Chef Take-Out Express favorites for takeaway or delivery, available on all platforms Uber Eats, Doordash, Grubhub and Toast.</p>
+                <p className='px-4  text-xl mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"'>Indian Chef Take-Out Express Motto is to bring to your home the aroma of India's rich culture, flavor, and culinary heritage. The eatery features selections of Indian meals to choose from and is the best choice for tasty Indian dishes. They come up with great and flavorful meals at worth-able prices, with outstanding experiences for patrons of all ages and historic towns like Umatilla. Whether you're craving a quick bite or looking to treat yourself to a delicious meal, you can order your Indian Chef Take-Out Express favorites for takeaway or delivery, available on all platforms Uber Eats, Doordash, Grubhub and Toast.</p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-3 px-12 justify-center gap-4'>
             {
@@ -38,6 +39,7 @@ const Home = () => {
             }
             </div>
             <Team></Team>
+            <Contact></Contact>
         </div>
     );
 };
